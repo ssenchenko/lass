9eb33b96bed230f50a008a4816e4c2b1d58617a2
Slava Senchenko <ssenchenko@gmail.com>
2025-09-06 14:58:21 -0700

    feat: add sveltekit

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { m } from "$lib/paraglide/messages.js"
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<h2>{m.hello_world({name: "Slava"})}</h2>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>

8326a83853d8c1f27982e1467f35a8131f100741
Slava Senchenko <ssenchenko@gmail.com>
2025-09-24 21:05:31 -0700

    feat: setup deployment from ci
    * chore: don't deploy unimportant stuff change
    
    * feat: add sveltekit (#3)
    
    * feat: add sveltekit
    
    * feat: setup deployment from ci (#5)
    
    * feat: setup deployment from ci
    
    * forgotten variable github.repo

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+	import { page } from '$app/state';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div style="display:none">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+	{/each}
+</div>
+
+{@render children?.()}

d14e5d30a24818925738ebca530dd0413a8c2bce
Slava Senchenko <ssenchenko@gmail.com>
2025-12-10 16:01:18 -0800

    fix css

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,7 +2,8 @@
     import { m } from "$lib/paraglide/messages.js"
 </script>
 
-
-<h1>Welcome to SvelteKit</h1>
-<h2>{m.hello_world({name: "Slava"})}</h2>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="prose">
+	<h1>Welcome to SvelteKit</h1>
+	<h2>{m.hello_world({name: "Slava"})}</h2>
+	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
